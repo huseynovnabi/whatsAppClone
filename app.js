4ca0186616bd5d7c432de3b21648ea74632ed050
huseynovnabi huseynovnabi <huseynovnabi2002@gmail.com>
2023-12-20 13:09:59 +0000

    change

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 "use strict";
 $(document).ready(function () {
+    $("#me").click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputme").val()) == "") {
+                let vall = $("#inputme").val(),
+                    newH3 = $('<h3></h3>'),
+                    newDiv = $('<div class="line"></div'),
+                    bigDiv = $(`<div ${myclass}></div`)
+                $(newH3).append(vall);
+                $(newDiv).append(newH3);
+                $(bigDiv).append(newDiv);
+                return bigDiv;
+            } else {
+                return
+            }
 
+        }
+        // $("#messageyou").append(myel());
+        $("#messageme").append(myel('class="end"'));
+        $("inputme").val("");
+        $("#messageme").scrollTo(0, $("#messageme").scrollHeight);
+        $("#messageyou").scrollTo(0, $("#messageyou").scrollHeight);
+    });
 });
-
