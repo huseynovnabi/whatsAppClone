74972be3b877a8c1b72ae8331da348b3c0e95aba
huseynovnabi huseynovnabi <huseynovnabi2002@gmail.com>
2023-12-21 11:38:44 +0000

    change

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
             if (!$.trim($("#inputme").val()) == "") {
                 let vall = $("#inputme").val(),
                     newH3 = $('<h3></h3>'),
-                    newDiv = $('<div class="line"></div'),
-                    bigDiv = $(`<div ${myclass}></div`)
+                    newDiv = $('<div class="line"></div>'),
+                    bigDiv = $(`<div ${myclass}></div>`)
                 $(newH3).append(vall);
                 $(newDiv).append(newH3);
                 $(bigDiv).append(newDiv);
@@ -18,8 +18,8 @@ $(document).ready(function () {
         }
         $("#messageyou").append(myel());
         $("#messageme").append(myel('class="end"'));
-        $("inputme").val("");
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight);
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight);
+        $("#inputme").val("");
+        document.querySelector("#messageme").scrollTop(0, document.querySelector("#messageme").scrollHeight);
+        document.querySelector("#messageyou").scrollTop(0, document.querySelector("#messageyou").scrollHeight);
     });
 });
