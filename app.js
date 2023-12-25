6767202193b68f49e57166a1c57c0a0c036ce1eb
huseynovnabi huseynovnabi <huseynovnabi2002@gmail.com>
2023-12-25 12:44:47 +0000

    changes

--- app.js
+++ app.js
@@ -6,18 +6,37 @@ $(document).ready(function () {
             if (!$.trim($("#inputMe").val()) == "") {
                 let vall = $("#inputMe").val(),
                     newLi = $('<li></li>'),
-                    newDiv = $('<div class="line"></div>'),
-                    bigDiv = $(`<div ${myclass}></div>`)
-                $(newH3).append(vall);
-                $(newDiv).append(newH3);
-                $(bigDiv).append(newDiv);
-                return bigDiv;
+                    newUl = $(`<ul ${myclass}></ul>`)
+                $(newLi).append(vall);
+                $(newUl).append(newLi);
+                return newUl;
             } else {
                 return
             }
         }
-        $("#messageyou").append(myel());
-        $("#messageme").append(myel('class="me"'));
-        $("#inputme").val("");
+        $("#ulYou").append(myel("class='lineYou'"));
+        $("#ulMe").append(myel('class="forUl lineMe"'));
+        $("#inputMe").val("");
+    });
+
+
+
+    $("#sendYou").click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputYou").val()) == "") {
+                let vall = $("#inputYou").val(),
+                    newLi = $('<li></li>'),
+                    newUl = $(`<ul ${myclass}></ul>`)
+                $(newLi).append(vall);
+                $(newUl).append(newLi);
+                return newUl;
+            } else {
+                return
+            }
+        }
+        $("#ulMe").append(myel("class='lineYou'"));
+        $("#ulYou").append(myel('class="forUl lineMe"'));
+        $("#inputYou").val("");
     });
 });
